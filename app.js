45891d3c693d87702ba1ae666dab5afd9a18c5a9
jotascript17 <joaowebdes@gmail.com>
2024-05-15 14:31:28 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Recado do João';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Myrella eu te AMO!';
+
+function verificarChute(){
+    console.log('o botao foi clicado')
+}
